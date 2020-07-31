695a4a77faa3347c76fceb1dce273576d1ae8662
tobao17 <phamtobao99@gmail.com>
2020-07-31 23:55:15 +0700

    Trafficligth to app

--- codersx-react-todo-list/src/App.js
+++ codersx-react-todo-list/src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 
 import './App.css';
-import TodoItem from './component/TodoItem'
+import TrafficLight from './component/TrafficLigth.js'
+const red=1;
+const yellow=2;
+const green=3; 
 class App extends React.Component {
  
-  constructor(){
-    super();
-    this.TodoItem=[{title:'đi chợ',isComplete:true},{title:'học tại coderx',isComplete:true},{title:'nấu ăn',isComplete:true}]
+  constructor()
+  {
+      super();
+      this.state={
+          currentColor: green
+      };
+      //console.log(this.state.currentColor)
+      setInterval(  ()=>{
+        this.setState({
+              currentColor:this.getNextColor(this.state.currentColor)
+          });
+      },1000)
+     
   }
+ 
+  getNextColor(color ){
+      //console.log(color)
+      if (color===red)
+         return yellow;
+      else if (color===yellow)
+        return green;
+      else
+       return red;
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          {
-            this.TodoItem.length>0 &&  this.TodoItem.map(item => <TodoItem item={item}></TodoItem>)
-          }
-          {
-            this.TodoItem.length===0 && 'Nothing Here!'
-          }
-           
+         <TrafficLight currentColor={this.state.currentColor}></TrafficLight>
         </header>
       </div>
     );
